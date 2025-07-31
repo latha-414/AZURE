15a1d13b45cdee24652aa5cff37e79de7f7175e8
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-31 15:26:46 +0530

    Update outputs.tf

--- modules/app-service/outputs.tf
+++ modules/app-service/outputs.tf
@@ -1,24 +1,14 @@
-output "app_service_name" {
-  description = "Name of the App Service"
-  value       = azurerm_linux_web_app.main.name
+output "app_service_id" {
+  description = "ID of the App Service"
+  value       = azurerm_linux_web_app.this.id
 }
 
 output "app_service_url" {
   description = "URL of the App Service"
-  value       = "https://${azurerm_linux_web_app.main.default_hostname}"
+  value       = "https://${azurerm_linux_web_app.this.default_hostname}"
 }
 
-output "app_service_id" {
-  description = "ID of the App Service"
-  value       = azurerm_linux_web_app.main.id
-}
-
-output "app_service_plan_id" {
+output "service_plan_id" {
   description = "ID of the App Service Plan"
-  value       = azurerm_service_plan.main.id
-}
-
-output "app_service_principal_id" {
-  description = "Principal ID of the App Service managed identity"
-  value       = azurerm_linux_web_app.main.identity[0].principal_id
+  value       = azurerm_service_plan.this.id
 }
