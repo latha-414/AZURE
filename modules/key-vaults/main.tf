af21bde2235e2754f6fc64fde2f1f9ef86e95c2c
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-31 15:23:42 +0530

    Update main.tf

--- modules/key-vaults/main.tf
+++ modules/key-vaults/main.tf
@@ -1,59 +1,24 @@
-# Get current client configuration
-data "azurerm_client_config" "current" {}
-
-# Key Vault
-resource "azurerm_key_vault" "main" {
-  name                        = "${var.key_vault_name}-${var.environment}"
-  location                    = var.location
-  resource_group_name         = var.resource_group_name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = var.soft_delete_retention_days
-  purge_protection_enabled    = var.purge_protection_enabled
-
-  sku_name = var.sku_name
+resource "azurerm_key_vault" "this" {
+  name                = var.key_vault_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = var.sku_name
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
     key_permissions = [
-      "Get", "List", "Create", "Delete", "Update", "Recover", "Purge"
+      "Get", "List", "Create", "Delete", "Update"
     ]
 
     secret_permissions = [
-      "Get", "List", "Set", "Delete", "Recover", "Purge"
-    ]
-
-    storage_permissions = [
-      "Get", "List", "Set", "Delete", "Recover", "Purge"
+      "Get", "List", "Set", "Delete"
     ]
   }
 
   tags = var.tags
 }
 
-# Key Vault Secrets
-resource "azurerm_key_vault_secret" "sql_admin_password" {
-  name         = "sql-admin-password"
-  value        = var.sql_admin_password
-  key_vault_id = azurerm_key_vault.main.id
-
-  depends_on = [azurerm_key_vault.main]
-}
-
-resource "azurerm_key_vault_secret" "app_connection_string" {
-  name         = "app-connection-string"
-  value        = var.app_connection_string
-  key_vault_id = azurerm_key_vault.main.id
-
-  depends_on = [azurerm_key_vault.main]
-}
-
-resource "azurerm_key_vault_secret" "function_app_key" {
-  name         = "function-app-key"
-  value        = var.function_app_key
-  key_vault_id = azurerm_key_vault.main.id
-
-  depends_on = [azurerm_key_vault.main]
-}
+data "azurerm_client_config" "current" {}
