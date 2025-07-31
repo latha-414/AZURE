7e692f07dba44064d38be258d82795a1526f495d
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-31 15:54:57 +0530

    Update main.tf

--- projects/prod/main.tf
+++ projects/prod/main.tf
@@ -1,105 +1,69 @@
-# Same structure as dev and qa, but for production
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      version = "~> 3.0"
+    }
+  }
+  
+  cloud {
+    organization = "temperhcp"
+    workspaces {
+      name = "project-prod"
     }
   }
 }
 
 provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = false  # Keep soft delete in production
-      recover_soft_deleted_key_vaults = true
-    }
-  }
+  features {}
 }
 
-# Resource Group Module
 module "resource_group" {
   source = "../../modules/resource-group"
   
   resource_group_name = var.resource_group_name
   location           = var.location
-  tags               = var.tags
+  tags              = var.tags
 }
 
-# Key Vault Module
 module "key_vault" {
-  source = "../../modules/key-vault"
+  source = "../../modules/key-vaults"
   
-  key_vault_name               = var.key_vault_name
-  environment                 = var.environment
-  resource_group_name         = module.resource_group.resource_group_name
-  location                   = module.resource_group.location
-  sql_admin_password         = var.sql_admin_password
-  app_connection_string      = var.app_connection_string
-  function_app_key           = var.function_app_key
-  purge_protection_enabled   = true  # Enable purge protection in production
-  soft_delete_retention_days = 90    # Longer retention in production
-  tags                       = var.tags
-  
-  depends_on = [module.resource_group]
+  key_vault_name      = var.key_vault_name
+  location           = module.resource_group.location
+  resource_group_name = module.resource_group.resource_group_name
+  tags              = var.tags
 }
 
-# SQL Database Module
 module "sql_database" {
   source = "../../modules/sql-database"
   
-  sql_server_name            = var.sql_server_name
-  database_name             = var.database_name
-  environment               = var.environment
-  resource_group_name       = module.resource_group.resource_group_name
-  location                 = module.resource_group.location
-  administrator_login       = var.sql_administrator_login
-  key_vault_name           = module.key_vault.key_vault_name
-  key_vault_resource_group = module.resource_group.resource_group_name
-  database_sku             = var.database_sku
-  max_size_gb              = var.database_max_size_gb
-  allowed_ip_ranges        = var.sql_allowed_ip_ranges
-  azuread_admin_login      = var.azuread_admin_login
-  azuread_admin_object_id  = var.azuread_admin_object_id
-  tags                     = var.tags
-  
-  depends_on = [module.resource_group, module.key_vault]
+  sql_server_name     = var.sql_server_name
+  database_name      = var.database_name
+  resource_group_name = module.resource_group.resource_group_name
+  location           = module.resource_group.location
+  admin_username     = var.sql_admin_username
+  admin_password     = var.sql_admin_password
+  tags              = var.tags
 }
 
-# App Service Module
 module "app_service" {
   source = "../../modules/app-service"
   
-  app_name                     = var.app_name
-  environment                  = var.environment
-  resource_group_name          = module.resource_group.resource_group_name
-  location                     = module.resource_group.location
-  app_service_plan_sku         = var.app_service_plan_sku
-  key_vault_name              = module.key_vault.key_vault_name
-  key_vault_resource_group    = module.resource_group.resource_group_name
-  app_settings                = var.app_settings
-  always_on                   = var.app_always_on
-  cors_allowed_origins        = var.cors_allowed_origins
-  tags                        = var.tags
-  
-  depends_on = [module.resource_group, module.key_vault, module.sql_database]
+  app_name           = var.app_service_name
+  service_plan_name  = var.app_service_plan_name
+  resource_group_name = module.resource_group.resource_group_name
+  location           = module.resource_group.location
+  tags              = var.tags
 }
 
-# Azure Functions Module
 module "azure_functions" {
   source = "../../modules/azure-functions"
   
-  function_app_name           = var.function_app_name
-  environment                = var.environment
-  resource_group_name        = module.resource_group.resource_group_name
-  location                  = module.resource_group.location
-  function_app_sku          = var.function_app_sku
-  key_vault_name            = module.key_vault.key_vault_name
-  key_vault_resource_group  = module.resource_group.resource_group_name
-  functions_worker_runtime  = var.functions_worker_runtime
-  app_settings              = var.function_app_settings
-  cors_allowed_origins      = var.function_cors_allowed_origins
-  tags                      = var.tags
-  
-  depends_on = [module.resource_group, module.key_vault]
+  function_app_name      = var.function_app_name
+  service_plan_name      = var.function_service_plan_name
+  storage_account_name   = var.function_storage_name
+  resource_group_name    = module.resource_group.resource_group_name
+  location              = module.resource_group.location
+  tags                 = var.tags
 }
