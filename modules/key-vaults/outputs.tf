6ec5f06e98314f908371cf23ddd8319a08f85477
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-31 15:24:26 +0530

    Update outputs.tf

--- modules/key-vaults/outputs.tf
+++ modules/key-vaults/outputs.tf
@@ -1,19 +1,9 @@
 output "key_vault_id" {
   description = "ID of the Key Vault"
-  value       = azurerm_key_vault.main.id
-}
-
-output "key_vault_name" {
-  description = "Name of the Key Vault"
-  value       = azurerm_key_vault.main.name
+  value       = azurerm_key_vault.this.id
 }
 
 output "key_vault_uri" {
   description = "URI of the Key Vault"
-  value       = azurerm_key_vault.main.vault_uri
-}
-
-output "sql_password_secret_id" {
-  description = "ID of the SQL password secret"
-  value       = azurerm_key_vault_secret.sql_admin_password.id
+  value       = azurerm_key_vault.this.vault_uri
 }
