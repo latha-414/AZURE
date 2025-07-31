d9c1fc56a877079d4078288ffe04dcfb3aca5a9a
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-31 15:26:28 +0530

    Update variables.tf

--- modules/app-service/variables.tf
+++ modules/app-service/variables.tf
@@ -1,10 +1,10 @@
 variable "app_name" {
-  description = "Name of the application"
+  description = "Name of the App Service"
   type        = string
 }
 
-variable "environment" {
-  description = "Environment name"
+variable "service_plan_name" {
+  description = "Name of the App Service Plan"
   type        = string
 }
 
@@ -18,50 +18,16 @@ variable "location" {
   type        = string
 }
 
-variable "app_service_plan_sku" {
-  description = "SKU for the App Service Plan"
+variable "sku_name" {
+  description = "SKU name for App Service Plan"
   type        = string
   default     = "B1"
 }
 
-variable "key_vault_name" {
-  description = "Name of the Key Vault"
-  type        = string
-}
-
-variable "key_vault_resource_group" {
-  description = "Resource group of the Key Vault"
-  type        = string
-}
-
 variable "app_settings" {
-  description = "Additional app settings"
+  description = "App settings for the web app"
   type        = map(string)
   default     = {}
-}
-
-variable "always_on" {
-  description = "Always on setting for App Service"
-  type        = bool
-  default     = false
-}
-
-variable "node_version" {
-  description = "Node.js version"
-  type        = string
-  default     = "18-lts"
-}
-
-variable "cors_allowed_origins" {
-  description = "CORS allowed origins"
-  type        = list(string)
-  default     = ["*"]
-}
-
-variable "cors_support_credentials" {
-  description = "CORS support credentials"
-  type        = bool
-  default     = false
 }
 
 variable "tags" {
